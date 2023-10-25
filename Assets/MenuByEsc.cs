7d6c505e4f9ac60853a65795ef0b78179d9bc6fc
KacperLipinski00 <kacperl2000@tlen.pl>
2023-10-25 10:54:43 +0200

    upload 2

--- Assets/MenuByEsc.cs
+++ Assets/MenuByEsc.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class SceneManagement : MonoBehaviour
+
+public class MenuByEsc : MonoBehaviour
 {
-    public string SceneName;
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void ChangeScene()
-    {
-        SceneManager.LoadScene(SceneName);
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MenuInGame");
+        }
     }
 }
--- Assets/Scene_start.cs
+++ Assets/Scene_start.cs
@@ -2,22 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class SceneManagement : MonoBehaviour
+
+public class Scene_start : MonoBehaviour
 {
-    public string SceneName;
     // Start is called before the first frame update
     void Start()
     {
-       
+        //SceneManager.UnloadScene("Gameplay");
+        //SceneManager.LoadScene("menu");
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    public void ChangeScene()
-    {
-        SceneManager.LoadScene(SceneName);
     }
 }
