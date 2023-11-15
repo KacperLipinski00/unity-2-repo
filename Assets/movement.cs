374a081e43d013aa62365e33457cb2e007b040ab
KacperLipinski00 <102984172+KacperLipinski00@users.noreply.github.com>
2023-11-15 09:24:02 +0100

    push v2

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,4 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
-{ }
+{
+    [SerializeField] float movementSpeed = 6f;
+
+    private float horizontal;
+    private float vertical;
+
+    public CharacterController characterController;
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        vertical = Input.GetAxis("Vertical") * movementSpeed;
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        characterController.Move(move * Time.deltaTime);
+
+    }
+}
